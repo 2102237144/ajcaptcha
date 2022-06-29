16d79f4fe03e81a760e01bcb43d2908c92f4e389
bruce <2777314125@qq.com>
2022-06-29 09:03:51 +0800

    pref: 新增aes检查密钥

--- src/Utils/AesUtils.php
+++ src/Utils/AesUtils.php
@@ -15,7 +15,7 @@ class AesUtils
      * @param $secretKey string 只有长度等于16位才能与前端CryptoJS加密一致
      * @return string
      */
-    public static function encrypt($str, $secretKey)
+    public static function encrypt($str, string $secretKey)
     {
         return base64_encode(openssl_encrypt($str, 'AES-128-ECB', $secretKey, OPENSSL_RAW_DATA));
 
@@ -24,12 +24,16 @@ class AesUtils
     /**
      * 解密
      * @param $str
-     * @param $secretKey
+     * @param $secretKey string 只有长度等于16位才能与前端CryptoJS加密一致
      * @return string
      */
-    public static function decrypt($str, $secretKey): string
+    public static function decrypt($str, string $secretKey): string
     {
-        return openssl_decrypt(base64_decode($str), 'AES-128-ECB', $secretKey,OPENSSL_RAW_DATA);
+        $ret = openssl_decrypt(base64_decode($str), 'AES-128-ECB', $secretKey,OPENSSL_RAW_DATA);
+        if($ret === false){
+            throw new \RuntimeException('请检查密钥是否正确！');
+        }
+        return $ret;
     }
 
 }
